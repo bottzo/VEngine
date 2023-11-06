4408d5d9e4cef0698f30b495b6bfb1675cd56e51
Jordi Bach i Balcells <jordibb18@TTC.CAT>
2023-11-06 21:02:32 +0100

    added glfw

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#define GLFW_INCLUDE_NONE
+#include "GLFW/glfw3.h"
 
 int main()
 {
-	printf("Hellow world");
+	if(!glfwInit())
+	{
+		printf("Mega F");
+		return 0;
+	}
+	printf("Good shit");
+	GLFWwindow* window = glfwCreateWindow(640, 480, "MyTitle", NULL, NULL);
+	if(!window)
+	{
+		printf("F window");
+		glfwTerminate();
+		return 0;
+	}
+	while(!glfwWindowShouldClose(window))
+	{
+	}
+	glfwTerminate();
 	return 0;
 }
