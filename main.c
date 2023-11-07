10d6c799b771d702e9778744f1d8ac136d2de721
Jordi Bach i Balcells <jordibb18@TTC.CAT>
2023-11-07 19:46:23 +0100

    unused glfw dirs + adding vulkan sdk

--- main.c
+++ main.c
@@ -1,22 +1,29 @@
-#include <stdio.h>
-#define GLFW_INCLUDE_NONE
+#define GLFW_INCLUDE_VULKAN
 #include "GLFW/glfw3.h"
+
+#include <stdio.h>
 
 int main()
 {
 	if(!glfwInit())
 	{
-		printf("Mega F");
+		printf("Mega F\n");
 		return 0;
 	}
-	printf("Good shit");
-	GLFWwindow* window = glfwCreateWindow(640, 480, "MyTitle", NULL, NULL);
+	printf("Good shit\n");
+	
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", NULL, NULL);
 	if(!window)
 	{
-		printf("F window");
+		printf("F window\n");
 		glfwTerminate();
 		return 0;
 	}
+
+	unsigned int extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, NULL);
+	printf("%u extensions supported\n", extensionCount);
 	while(!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
